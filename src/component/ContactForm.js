81dd0f0da5e4fb45c56d9d3dc8d7d3bbe87b6596
boyeon kim <hajoce02@naver.com>
2024-08-27 02:32:15 +0900

    first commit

---
+++ src/component/ContactForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const ContactForm = () => {
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>이름</Form.Label>
+          <Form.Control type="text" placeholder="이름을 입력해주세요" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formContact">
+          <Form.Label>전화번호</Form.Label>
+          <Form.Control type="number" placeholder="전화번호를 입력해주세요" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default ContactForm;
